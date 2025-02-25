64c7a6e975a336653e5a599c145a3c5eff34948b
Alan Jeferson Macias Mamani <alanmacias2340@gmail.com>
2025-02-25 18:26:01 -0400

    Añadir componente NavBar con logo y enlaces de navegación

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Logo from '../../../public/logo.png'
+
+type Props = {}
+
+function NavBar({}: Props) {
+  return (
+    <div className='flex bg-blue-200 justify-between items-center'>
+        <div className='flex justify-center items-center p-5 gap-2.5'>
+            <img src={Logo} alt="logo" className='h-20 w-20 bg-no-repeat bg-center bg-cover' />
+            <h1 className='text-gray-50 font-black text-3xl'>SoftPublic</h1>
+        </div>
+        <div className='flex justify-center items-center p-5 gap-2.5'>
+            <ul className='flex justify-center items-center gap-5 text-blue-950 font-medium'>
+                <li className='hover:text-blue-50 cursor-pointer'>Inicio</li>
+                <li className='hover:text-blue-50 cursor-pointer'>Publicaciones</li>
+                <li className='hover:text-blue-50 cursor-pointer'>Sobre nosotros</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
