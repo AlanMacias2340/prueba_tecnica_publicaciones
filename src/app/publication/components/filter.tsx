39735e5f679ad338301ff62bec359986bf36ef68
Alan Jeferson Macias Mamani <alanmacias2340@gmail.com>
2025-02-25 18:26:10 -0400

    Añadir funcionalidad de filtrado en el componente Filter con búsqueda y ordenación

--- src/app/publication/components/filter.tsx
+++ src/app/publication/components/filter.tsx
@@ -1,12 +1,33 @@
+import IconeSearch from "../../../assets/search_icone.png";
 
-type Props = {}
+type Props = {};
 
 function Filter({}: Props) {
   return (
-    <div>
-        
+    <div className="flex justify-around items-center w-full">
+      <div className="flex justify-center items-center gap-3 p-5 w-[60%]">
+        {/**filtrar por titulo o contenido */}
+        <input
+          type="text"
+          placeholder="Buscador"
+          className="border-2 border-blue-950 rounded-md p-1 w-[100%]"
+        />
+        <img src={IconeSearch} alt="btn_search" className="min-h-10 min-w-10 max-w-11 max-h-11 cursor-pointer" />
+      </div>
+      <div className="w-[30%] flex justify-center items-center gap-5 p-5">
+        {/**filtrar por orden alfabetico */}
+        <label htmlFor="order">Ordenar por:</label>
+        <select
+          name="order"
+          id="order"
+          className="border-2 border-blue-950 rounded-md p-1"
+        >
+          <option value="asc">A - Z</option>
+          <option value="desc">Z - A</option>
+        </select>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Filter
+export default Filter;
