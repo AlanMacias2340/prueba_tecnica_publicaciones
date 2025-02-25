89ec7015cfc443d478336d1251b1d76f033fa8e9
Alan Jeferson Macias Mamani <alanmacias2340@gmail.com>
2025-02-25 19:47:57 -0400

    Añadir estado y funcionalidad de búsqueda en el contexto de publicaciones

--- src/app/publication/publication.tsx
+++ src/app/publication/publication.tsx
@@ -1,25 +1,43 @@
-import { createContext, useEffect } from "react";
-import Filter  from "./components/filter";
+import { createContext, useEffect, useState } from "react";
+import Filter from "./components/filter";
+import ListPublication from "./components/list_publication";
 
+type PublicationContextProps = {
+  filteredPosts?: any[];
+  toggleSearch?: (e: any) => void;
+};
 
-type PublicationContextProps = {};
-
-export const PublicationContext = createContext<PublicationContextProps>({});
+export const PublicationContext = createContext<PublicationContextProps>({
+  filteredPosts: [],
+  toggleSearch: () => {},
+});
 
 type Props = {};
 
 function Publication({}: Props) {
+  const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => setPosts(json))
+      .catch((error) => console.log(error));
   }, []);
 
-  
+  const filteredPosts = posts.filter((post: { title: string }) => 
+    post.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const toggleSearch = (e: any) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
-    <PublicationContext.Provider value={{}}>
+    <PublicationContext.Provider value={{filteredPosts, toggleSearch}}>
       <Filter />
+      <ListPublication />
     </PublicationContext.Provider>
   );
 }
