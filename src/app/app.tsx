cafe36b14ff82e91ab1ee29da54bfe6c0bf54024
Alan Jeferson Macias Mamani <alanmacias2340@gmail.com>
2025-02-25 17:10:18 -0400

    Añadir el componente App para renderizar Publication.

---
+++ src/app/app.tsx
@@ -0,0 +1,13 @@
+import Publication from './publication/publication'
+
+type Props = {}
+
+function App({}: Props) {
+  return (
+    <>
+        <Publication />
+    </>
+  )
+}
+
+export default App
