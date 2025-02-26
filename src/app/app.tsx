e650e46cf9ffd144efd30352898dceddb1311200
Alan Jeferson Macias Mamani <alanmacias2340@gmail.com>
2025-02-25 21:21:18 -0400

    Añadir enrutamiento con react-router-dom y componentes de detalle y pie de página

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Publication from "./publication/publication";
 import NavBar from "./components/navbar";
+import Footer from "./components/footer";
+import PostDetail from "./post_detail/post_detail";
 
 type Props = {};
 
 function App({}: Props) {
   return (
-    <>
+    <Router>
       <NavBar />
-      <Publication />
-    </>
+      <Routes>
+        <Route path="/publication" element={<Publication />} />
+        <Route path="/post_detail/:id" element={<PostDetail />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
