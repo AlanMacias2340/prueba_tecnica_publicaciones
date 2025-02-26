3cf6ad5fd0234cf29d9675955d2af4a6046a545f
Alan Jeferson Macias Mamani <alanmacias2340@gmail.com>
2025-02-25 21:21:24 -0400

    Añadir componente Footer con estilo y contenido de copyright

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+function Footer({}: Props) {
+  return (
+    <footer className="flex justify-center items-center bg-blue-200">
+      <p className="text-gray-50 font-medium p-5">© 2021 SoftPublic</p>
+    </footer>
+  );
+}
+
+export default Footer;
