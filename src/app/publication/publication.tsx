cff7394161b01b74d2e923453e2c35b19e14dd9b
Alan Jeferson Macias Mamani <alanmacias2340@gmail.com>
2025-02-25 20:03:57 -0400

    Añadir funcionalidad de ordenamiento y búsqueda en el contexto de publicaciones

--- src/app/publication/publication.tsx
+++ src/app/publication/publication.tsx
@@ -3,13 +3,15 @@ import Filter from "./components/filter";
 import ListPublication from "./components/list_publication";
 
 type PublicationContextProps = {
-  filteredPosts?: any[];
+  sortedPosts?: any[];
   toggleSearch?: (e: any) => void;
+  toggleSort?: (e: any) => void;
 };
 
 export const PublicationContext = createContext<PublicationContextProps>({
-  filteredPosts: [],
+  sortedPosts: [],
   toggleSearch: () => {},
+  toggleSort: () => {},
 });
 
 type Props = {};
@@ -17,6 +19,7 @@ type Props = {};
 function Publication({}: Props) {
   const [posts, setPosts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
 
 
   useEffect(() => {
@@ -26,16 +29,28 @@ function Publication({}: Props) {
       .catch((error) => console.log(error));
   }, []);
 
-  const filteredPosts = posts.filter((post: { title: string }) => 
-    post.title.toLowerCase().includes(searchTerm.toLowerCase())
+  const filteredPosts = posts.filter((post: { title: string, body: string }) => 
+    post.title.toLowerCase().includes(searchTerm.toLowerCase()) || post.body.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  const sortedPosts = [...filteredPosts].sort((a: { title: string }, b: { title: string }) => {
+    if (sortOrder === "asc") {
+      return a.title.localeCompare(b.title); // Sort by title (A → Z)
+    } else {
+      return b.title.localeCompare(a.title); // Sort by title (Z → A)
+    }
+  });
 
   const toggleSearch = (e: any) => {
     setSearchTerm(e.target.value);
   };
 
+  const toggleSort = (e: any) => {
+    setSortOrder(e.target.value);
+  };
+
   return (
-    <PublicationContext.Provider value={{filteredPosts, toggleSearch}}>
+    <PublicationContext.Provider value={{sortedPosts, toggleSearch, toggleSort}}>
       <Filter />
       <ListPublication />
     </PublicationContext.Provider>
