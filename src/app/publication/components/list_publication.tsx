7d3f0da990c0b448c51b840b9c64f64ad8a021f7
Alan Jeferson Macias Mamani <alanmacias2340@gmail.com>
2025-02-25 21:59:00 -0400

    Ajustar la altura máxima del título en list_publication.tsx para mejorar la presentación

--- src/app/publication/components/list_publication.tsx
+++ src/app/publication/components/list_publication.tsx
@@ -18,7 +18,7 @@ function ListPublication({beggining, end}: Props) {
     <div className="grid grid-cols-3 gap-5 p-6">
       {sortedPosts?.slice(beggining, end).map((post: { id: number; title: string; body: string }) => (
         <div key={post.id} className="flex flex-col gap-3 bg-blue-400 p-8 pl-10 pr-10 rounded-2xl text-white cursor-pointer duration-200 min-h-60 max-h-60 hover:bg-blue-500" onClick={() => navigate(`/post_detail/${post.id}`)}>
-          <h3 className="font-black text-xl h-14 min-h-14">{post.title}</h3>
+          <h3 className="font-black text-xl max-h-24 min-h-14">{post.title}</h3>
           <p className="font-medium">{post.body}</p>
         </div>
       ))}
