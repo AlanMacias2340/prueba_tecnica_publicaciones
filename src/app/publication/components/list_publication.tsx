45c85825fb79f47cae307218e713a612f89b265c
Alan Jeferson Macias Mamani <alanmacias2340@gmail.com>
2025-02-25 20:31:18 -0400

    Actualizar el componente ListPublication para aceptar parámetros de paginación

--- src/app/publication/components/list_publication.tsx
+++ src/app/publication/components/list_publication.tsx
@@ -3,11 +3,12 @@ import { PublicationContext } from "../publication";
 
 
 type Props = {
+  beggining: number;
+  end: number;
 };
 
-function ListPublication({}: Props) {
+function ListPublication({beggining, end}: Props) {
   const {sortedPosts} = useContext(PublicationContext);
-
 
 
   return (
@@ -16,7 +17,7 @@ function ListPublication({}: Props) {
         <h3 className="font-black text-xl">Hola mundo</h3>
         <p className="font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit perferendis dolore velit reprehenderit commodi beatae at minus voluptatem qui. Quae perspiciatis excepturi quo sed sunt repellat veniam incidunt tempora eaque.</p>
       </div> */}
-      {sortedPosts?.slice(0, 12).map((post: { id: number; title: string; body: string }) => (
+      {sortedPosts?.slice(beggining, end).map((post: { id: number; title: string; body: string }) => (
         <div key={post.id} className="flex flex-col gap-3 bg-blue-400 p-8 pl-10 pr-10 rounded-2xl text-white cursor-pointer duration-200 hover:bg-blue-500">
           <h3 className="font-black text-xl">{post.title}</h3>
           <p className="font-medium">{post.body}</p>
